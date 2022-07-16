ab0f36da0ead1768d2a9a9bc9493ddcf6b551bdd
zeynab <zeynab.mvs@gmail.com>
2022-07-16 21:12:17 +0430

    header styles unfinished

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,33 @@
 console.log("hello from script.js");
+
+// Header open and close hamburgur menu
+const btnHamburgur = document.querySelector('#jsbtnHamburgur')
+const header = document.querySelector('.header')
+const fadeElems = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
+const overlay = document.querySelector('.overlay')
+const logo = document.querySelector('.header__logo')
+
+btnHamburgur.addEventListener('click', function(){ 
+    console.log(fadeElems)
+    
+    if(header.classList.contains('open')){ // Close hamburgur menu
+        body.classList.remove('noscroll')
+        header.classList.remove('open')
+        logo.classList.remove('visually-hidden')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        });
+    } 
+    else { // Open hamburgur menu
+        body.classList.add('noscroll')
+        header.classList.add('open') 
+        logo.classList.add('visually-hidden')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+            
+        });
+    } 
+})
